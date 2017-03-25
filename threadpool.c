8fa73e63b5263d4ccce359fb506c74de0f1648e3
Kevin Liu <minghui.liu@trincoll.edu>
2017-03-25 18:35:49 -0400

    jobqueue definition and creation

--- threadpool.c
+++ threadpool.c
@@ -8,25 +8,26 @@
 
 #include "threadpool.h"
 
-void thread_create(thread_t *thread);
-void thread_destroy(thread_t *thread);
-void *thread_idle(void *pkg);
-
-
 /*
  * threadpool_create - create a thread pool of size threads
  */
 threadpool_t *threadpool_create(size_t num_threads) {
 	threadpool_t *thpool;
 	
+	/* allocate space for thread pool */
 	thpool = (threadpool_t *)malloc(sizeof(threadpool_t));
 	if (thpool == NULL) {
 		fprintf(stderr, "threadpool_create(): Could not allocate memory for thread pool\n");
 		return NULL;
 	}
 	thpool->num_threads = num_threads;
-	
-	/* allocate space for pthreads */
+
+	/* allocate space for jobqueue */
+	thpool->jobqueue = (jobqueue_t *)malloc(sizeof(jobqueue_t));
+	thpool->jobqueue->size = 16; 
+	thpool->jobqueue->queue = (job_t *)malloc(thpool->jobqueue->size * sizeof(job_t));
+
+	/* allocate space for threads */
 	thpool->threads = (thread_t *)malloc(num_threads * sizeof(thread_t));
 	if (thpool->threads == NULL) {
 		fprintf(stderr, "threadpool_create(): Could not allocate memory for threads\n");
@@ -48,22 +49,35 @@ threadpool_t *threadpool_create(size_t num_threads) {
 	return thpool;
 }
 
+/*
+ * thread_create - create one thread
+ */
 void thread_create(thread_t *thread) {
 	if (pthread_create(&(thread->pthread_handle), NULL, thread_idle, (void *)thread->id)) {
 		fprintf(stderr, "thread_create(): Could not create thread\n");
 	}
 }
 
+/*
+ * thread_idle - the function that threads run when there is no work to do
+ */
 void *thread_idle(void *pkg) {
 	long id = (long)pkg;
 	printf("thread %ld finished\n", id);
 }
 
+/*
+ * thread_destroy - destroy a single thread
+ */
 void thread_destroy(thread_t *thread) {
 	pthread_join(thread->pthread_handle, NULL);
 	printf("joined thread %d\n", thread->id);
 }
 
+/*
+ * threadpool_destroy - wait for threads to finish their work and destroy 
+ *   the thread pool
+ */
 void threadpool_destroy(threadpool_t *thpool) {
 	int i;
 	for (i = 0; i < thpool->num_threads; i++) {
--- threadpool.h
+++ threadpool.h
@@ -7,16 +7,34 @@
 
 #include <pthread.h>
 
+/* structure */
 typedef struct thread_t {
 	int id;
 	pthread_t pthread_handle;
 } thread_t;
 
+typedef struct job_t {
+	void (*function)(void *pkg);	/* function pointer */
+} job_t;
+
+typedef struct jobqueue_t {
+	job_t *queue;
+	int front;
+	int nextempty;
+	int size;
+} jobqueue_t;
+
 typedef struct threadpool_t {
 	int num_threads;
 	thread_t *threads;
-
+	jobqueue_t *jobqueue;
 } threadpool_t;
 
+/* prototypes */
+threadpool_t *threadpool_create(size_t num_threads);
+void threadpool_destroy(threadpool_t *thpool);
+void thread_create(thread_t *thread);
+void thread_destroy(thread_t *thread);
+void *thread_idle(void *pkg);
 
 #endif
